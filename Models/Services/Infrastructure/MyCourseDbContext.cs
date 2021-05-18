a2704d93e742f2ca0d6e48fcc461a563b46e546f
Pietro <pezzotta.pietro.pp@gmail.com>
2021-05-18 23:00:04 +0200

    Mappare le proprietà complesse
    owned types e relazioni

--- Models/Services/Infrastructure/MyCourseDbContext.cs
+++ Models/Services/Infrastructure/MyCourseDbContext.cs
@@ -33,10 +33,28 @@ namespace MyCourse.Models.Entities
 
             modelBuilder.Entity<Course>(entity =>
             {
-                entity.ToTable("Courses");//superfluo se la tabella si chiama come la proprietà che espone il db
-                entity.HasKey(course => course.Id); //superlfuo se la proprietà si chiama id o nome entita con suffisso id
-                //entity.HasKey(course => {course.Id, course.Author}); se ha più chiavi primarie
+                entity.ToTable("Courses"); //Superfluo se la tabella si chiama come la proprietà che espone il DbSet
+                entity.HasKey(course => course.Id); //Superfluo se la proprietà si chiama Id oppure CoursesId
+                //entity.HasKey(course => new { course.Id, course.Author }); //Per chiavi primarie composite (è importante rispettare l'ordine dei campi)
 
+                //Mapping per gli owned types
+                entity.OwnsOne(course => course.CurrentPrice, builder =>
+                {
+                    builder.Property(money => money.Currency)
+                    .HasConversion<string>()
+                    .HasColumnName("CurrentPrice_Currency"); //Superfluo perché le nostre colonne seguono già la convenzione di nomi
+                    builder.Property(money => money.Amount).HasColumnName("CurrentPrice_Amount"); //Superfluo perché le nostre colonne seguono già la convenzione di nomi
+                });
+
+                entity.OwnsOne(course => course.FullPrice, builder =>
+                {
+                    builder.Property(money => money.Currency).HasConversion<string>();
+                });
+
+                //Mapping per le relazioni
+                entity.HasMany(course => course.Lessons)
+                      .WithOne(lesson => lesson.Course)
+                      .HasForeignKey(lesson => lesson.CourseId); //Superflua se la proprietà si chiama CourseId
 
                 #region Mapping generato automaticamente dal tool di reverse engineering
                 /*
