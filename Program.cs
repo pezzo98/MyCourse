b93d0d94e006067ad0b6d1e6d7bf44bd60cc0fbc
Pietro <pezzotta.pietro.pp@gmail.com>
2021-07-28 23:55:11 +0200

    inizio migrazione .net 3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace MyCourse
 {
@@ -17,23 +10,11 @@ namespace MyCourse
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                //Posso ridefinire l'elenco dei provider di default
-                /*.ConfigureLogging((context, builder) => {
-                    builder.ClearProviders();
-                    builder.AddConsole();
-                    builder.Add...;
-                })*/
-
-                //Posso ridefinire l'elenco delle fonti di configurazione con ConfigureAppConfiguration
-                /*.ConfigureAppConfiguration((context, builder) => {
-                    builder.Sources.Clear();
-                    builder.AddJsonFile("appsettings.json", optional:true, reloadOnChange: true);
-                    builder.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-                    //Qui altre fonti...
-                })*/
-
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webHostBuilder =>
+            {
+                webHostBuilder.UseStartup<Startup>();
+            });
     }
 }
